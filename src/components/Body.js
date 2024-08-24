e8939f69d687ce0b2ce066cfce69473d0b44904e
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-24 18:19:40 +0530

    Exploring the world

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,70 @@
-import { swiggyData } from "../utils/data";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const resData = swiggyData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-  const [topRatedRestaurants, setTopRatedRestaurants] = useState(resData);
+  const [restaurants, setRestaurants] = useState([]);
+  const [filterenRestaurests, setFilterenRestaurests] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.4749252&lng=73.9374517&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const data = await response.json();
+      setRestaurants(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilterenRestaurests(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleFilter = () => {
-    const filterenRestaurests = topRatedRestaurants.filter((res) => res?.info?.avgRating >= 4.5);
-    console.log(filterenRestaurests);    
-    setTopRatedRestaurants(filterenRestaurests);
+    const filterenRes = restaurants.filter(
+      (res) => res?.info?.avgRating >= 4.5
+    );
+    setFilterenRestaurests(filterenRes);
+  };
+
+  const handleSearch = () => {
+    const filterenRes = restaurants.filter(
+      (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilterenRestaurests(filterenRes);
+  }
+
+  if (restaurants.length === 0) {
+    return <Shimmer />;
   }
 
   return (
     <div className="body">
       <div className="filter">
-        <button 
-          className="filter-btn"
-          onClick={handleFilter}
-        >
+        <div className="search">
+          <input 
+            type="text" 
+            className="search-box" 
+            value={searchText} 
+            onChange={(e) => setSearchText(e.target.value)}  
+          />
+          <button 
+            className="search-btn"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+        <button className="filter-btn" onClick={handleFilter}>
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {topRatedRestaurants.map((data) => (
+        {filterenRestaurests.map((data) => (
           <RestaurantCard
             key={data.info.id}
             name={data.info.name}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [login, setLogin] = useState("Login");
+  const handleLogin = () => {
+    login === "Login" ? setLogin("Logout") : setLogin("Login");
+  };
   return (
     <div className="header">
       <div className="nav-bar">
         <div>
-          <img
-            className="logo"
-            src={LOGO_URL}
-            alt="restaurent logo"
-          />
+          <img className="logo" src={LOGO_URL} alt="restaurent logo" />
         </div>
         <div className="menu">
           <ul className="menu-list">
@@ -17,6 +18,9 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button className="login" onClick={handleLogin}>
+              {login}
+            </button>
           </ul>
         </div>
       </div>
